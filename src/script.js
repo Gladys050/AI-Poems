a6729a11e104340413ea5b16bd527a4522908ef8
Gladys050 <Monnapulegladys@gmail.com>
2026-03-25 01:58:36 +0200

    added a basic javascript

---
+++ src/script.js
@@ -0,0 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "La tombe dit à la rose",
+    autStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
